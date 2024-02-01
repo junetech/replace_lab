bef0da77a270e6bbb6db021e8248ca7cc2fc8261
JuneTech <junetech0@gmail.com>
2024-02-01 22:51:54 +0900

    ISPD 2005 Tcl files

---
+++ test/adaptec4.tcl
@@ -0,0 +1,24 @@
+set design adaptec4_lefdef
+set bench_dir ./ispd2005/adaptec4/
+
+replace_external rep
+
+# Import LEF/DEF files
+rep import_lef ${bench_dir}/${design}.lef
+rep import_def ${bench_dir}/${design}.def
+rep set_output ./output/
+
+# Initialize RePlAce
+rep init_replace
+
+# place_cell with Nesterov method
+rep place_cell_nesterov_place
+
+# Export DEF file
+rep export_def ./${design}_placed.def
+
+puts "nesterovPlace HPWL: [rep get_hpwl]"
+puts "final WNS: [rep get_wns]"
+puts "final TNS: [rep get_tns]"
+
+exit
